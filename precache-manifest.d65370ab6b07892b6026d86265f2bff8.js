eb4f8aab30849f4335d46137bca1439f627dfca5
ninadthepade <ninad.thepade@gmail.com>
2020-05-05 00:24:22 +0530

    Updates

---
+++ precache-manifest.d65370ab6b07892b6026d86265f2bff8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c1d4772b587baa802e8a61cb5ac457",
+    "url": "/quizapp/index.html"
+  },
+  {
+    "revision": "631edd58bf6d521e906c",
+    "url": "/quizapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5c38c05e14c2892ba5df",
+    "url": "/quizapp/static/js/2.3ed2fa08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quizapp/static/js/2.3ed2fa08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "631edd58bf6d521e906c",
+    "url": "/quizapp/static/js/main.67ce909f.chunk.js"
+  },
+  {
+    "revision": "4a64fee4b188f18b6a1d",
+    "url": "/quizapp/static/js/runtime-main.da13a292.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quizapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
